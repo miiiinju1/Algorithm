cf02fc0b30b384f379fcf464905b9a13a030a1a8
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-11-09 21:04:03 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.14 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=java#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 72.1 MB, 시간: 0.11 ms
+메모리: 79.5 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 20:45:48
+2023년 11월 4일 21:3:59
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,46 +1,18 @@
-import java.util.*;
 class Solution {
-    static HashSet<Integer> set = new HashSet<>();
-    static int maxIndex = 2;
     public boolean 소수 (String str) {
         long number = Long.parseLong(str);
         if(number==1)
             return false;
-        
-        long sqrtNumber = (long)Math.sqrt(number)+1;
-        
-        int i=maxIndex;
-        for(;i<sqrtNumber;i++) {
-            if(set.contains(i)) {
-                
-                maxIndex =i;
-                return false;
-            }
-            
+        long sqrtNumber = (long)Math.sqrt(number);
+        for(int i = 2;i<=sqrtNumber;i++) {
             if(number%i==0) {
-                set.add(i);
-                maxIndex =i;
                 return false;
             }
         }
-        maxIndex =i;
         return true;
-        
-        
     }
-    public int solution(int n, int k) {
-        StringBuilder sb = new StringBuilder();
-        while(true) {
-            int 나머지 = n%k;
-            sb.insert(0,나머지); 
-            n = n/k;
-            if(n==0) {      
-                break;
-            }
-        }
-        // System.out.println(sb);
-        String[] strArray = sb.toString().split("0");
-        
+    public int solution(int n, int k) { 
+        String[] strArray = Integer.toString(n,k).split("0");
         int answer=0;
         for(String str : strArray) {
             if(!str.equals("")&&소수(str)) {
