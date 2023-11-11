97b4919a8daef4ad37e71e6eb51bd3052ac6516f
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-11-11 21:49:37 +0900

    [level 2] Title: 괄호 변환, Time: 0.22 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,82 @@
+class Solution {
+    static boolean isCorrect (String str) {
+        int left = 0,right = 0;
+        for(int i =0;i<str.length();i++) {
+            if(str.charAt(i)=='(') {
+                    left++;
+                }
+                else {
+                    right++;
+                }
+                if(left<right)
+                    return false;
+                else if(left == right)
+                    return true;
+
+            }
+            return false;
+    }
+        
+    
+    static void change (String str) {
+        
+        
+    }
+    static StringBuilder sb =new StringBuilder();
+    
+    static int split(String str) {
+        int left=0,right = 0;
+        for(int i =0;i<str.length();i++) {
+            if(str.charAt(i)=='(') {
+                left++;
+            }
+            else {
+                right++;
+            }
+            
+            if(left==right)
+                return i+1;
+        }
+        return -1;
+    }
+    
+    static String execute(String p) {
+        StringBuilder temp = new StringBuilder();
+        int splitPoint = split(p);
+        if(splitPoint==-1) {
+            return "";
+        }
+        String u = p.substring(0,splitPoint);
+        String v = p.substring(splitPoint);
+       if(isCorrect(u)) {
+           sb.append(u);
+           sb.append(execute(v));
+       }
+       else {
+           sb.append("(");
+           
+           sb.append(execute(v));
+           
+           sb.append(")");
+           String deleted = u.substring(1,u.length()-1);
+           
+           for(char c : deleted.toCharArray()) {
+               if(c=='(') {
+                   sb.append(")");
+               }
+               else {
+                   sb.append("(");
+               }
+           }
+       } 
+        
+        return temp.toString();
+    }
+    
+    
+    public String solution(String p) {
+        
+        execute(p);
+        return sb.toString();
+    }
+}
