cc0354f6e9fc06ac9459b17705666d65013c26ce
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-11-16 17:24:31 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 41.02 ms, Memory: 219 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,96 @@
+class Solution {
+    static int[][] board;
+    static void attack(int[] skill) {
+        int r1 = skill[1];
+        int c1 = skill[2];
+        int r2 = skill[3];
+        int c2 = skill[4];
+        int degree = skill[5];
+        
+        
+/*
+-2 0 0 2
+0      0
+0      0
+0      0
+2 0 0 -2
+*/
+        
+        /*
+        -2 0 2
+        0 0 0
+        0 0 0
+        */
+        board[r1][c1] -= degree;
+        r2 +=1;
+        c2+=1;
+        if(c2<board[0].length){ //시작 x좌표가 넘지 않는 격ㅇ우
+            board[r1][c2] += degree;
+        }
+        if(r2<board.length) 
+            board[r2][c1] +=degree;
+        
+        if((c2<board[0].length)&&(r2<board.length))
+            board[r2][c2]-=degree;
+      
+    
+        
+          
+    }
+    static void heal (int[] skill) {
+        int r1 = skill[1];
+        int c1 = skill[2];
+        int r2 = skill[3];
+        int c2 = skill[4];
+        int degree = skill[5];
+        board[r1][c1] += degree;
+        r2 +=1;
+        c2+=1;
+        if(c2<board[0].length){ //시작 x좌표가 넘지 않는 격ㅇ우
+            board[r1][c2] -= degree;
+            
+        }
+        if(r2<board.length) 
+            board[r2][c1] -=degree;
+        if((c2<board[0].length)&&(r2<board.length))
+            board[r2][c2]+=degree;
+      
+     
+        
+    }
+    public int solution(int[][] b, int[][] skills) {
+        board  = new int[b.length][b[0].length];
+        for(int[] skill : skills) {
+            if(skill[0]==1) {//적의 공격
+                attack(skill);
+            }
+            else {
+                heal(skill);
+            }
+        }
+        int count=0;
+      
+        
+        for(int i=0;i<b.length;i++) {
+            for(int j= 1;j<b[0].length;j++) {
+                board[i][j] += board[i][j-1]; 
+            }
+        } 
+        for(int j= 0;j<b[0].length;j++) {
+            for(int i=1;i<b.length;i++) {
+                board[i][j] += board[i-1][j]; 
+            }
+        }
+        for(int i=0;i<b.length;i++) {
+            for(int j= 0;j<b[0].length;j++) {
+                b[i][j]+=board[i][j];
+                if(b[i][j]>0)
+                    count++;
+            }
+        }
+        
+          
+        int answer = count;
+        return answer;
+    }
+}
