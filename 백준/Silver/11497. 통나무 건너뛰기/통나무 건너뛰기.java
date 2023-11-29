67175a21107bf59291344e0ff384c6c19059c97c
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-11-29 10:38:41 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 556 ms, Memory: 48668 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -1,6 +1,5 @@
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -9,7 +8,6 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
-
         for (int tc = 0; tc < T; tc++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
@@ -19,39 +17,15 @@ public class Main {
                 ary[i] = Integer.parseInt(st.nextToken());
             }
             Arrays.sort(ary);
-            ArrayList<Integer> list = new ArrayList<>();
-
-            list.add(ary[0]);
-            list.add(ary[1]);
-            int result = Math.abs(ary[0] - ary[1]);
-            int minDiff = Integer.MAX_VALUE;
-            for(int i=2;i<N;i++) {
-                int now = ary[i];
-                minDiff = Integer.MAX_VALUE;
-                int index = 0;
-                int max = 0;
-                int length = list.size();
-                for (int j = 0; j < length - 1; j++) {
-                    int left = Math.abs(list.get(j) - now);
-                    int right = Math.abs(list.get(j+1) - now);
-                    int temp = left + right;
-                    if (temp < minDiff) {
-                        minDiff = temp;
-                        max = Math.max(left, right);
-                        index = j+1;
-                    }
-                    if (minDiff != Integer.MAX_VALUE && temp > minDiff) {
-                        break;
-                    }
-
-
-                }
-                result = Math.max(max,result);
-                list.add(index, now);
+            int result = 0;
+            for (int i = 0; i < N-2; i += 2) {
+                result = Math.max(result, Math.abs(ary[i] - ary[i + 2]));
             }
-            bw.write( result+"\n");
+            for (int i = 1; i < N-2; i += 2) {
+                result = Math.max(result, Math.abs(ary[i] - ary[i + 2]));
             }
-
+            bw.write(result + "\n");
+        }
         bw.flush();
         bw.close();
     }
