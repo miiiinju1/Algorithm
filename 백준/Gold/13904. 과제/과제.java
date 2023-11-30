206908b05f8c0c3fe26bfa03532cc026330df446
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-11-30 14:42:15 +0900

    [Gold III] Title: 과제, Time: 140 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -1,65 +1,55 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Assignment implements Comparable<Assignment>{
-        int d,w;
+    private static class Assignment implements Comparable<Assignment> {
+        int due;
+        int weight;
 
-        public Assignment(int d, int w) {
-            this.d = d;
-            this.w = w;
+        public Assignment(int due, int weight) {
+            this.due = due;
+            this.weight = weight;
         }
 
         @Override
-        public int compareTo(Assignment o) {
-            if(this.w==o.w)
-                return o.d - this.d;
-            return o.w - this.w;
+        public int compareTo(Assignment other) {
+            return other.weight - this.weight; // 점수가 높은 과제 순으로 정렬
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 과제 개수
 
+        List<Assignment> assignmentList = new ArrayList<>();
+        boolean[] occupied = new boolean[1001]; // 각 날짜에 과제가 잡혔는지 여부
+        int ans = 0; // 총 점수
 
-        List<Assignment> list = new ArrayList<>();
-        int max = 0;
         for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-            int d = Integer.parseInt(input[0]);
-            int w = Integer.parseInt(input[1]);
-            list.add(new Assignment(d, w));
-            if(max<d)
-                max = d;
-
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int due = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            assignmentList.add(new Assignment(due, weight));
         }
-        int sum = 0;
-        AtomicInteger day =new AtomicInteger(max);
-        List<Assignment> temp;
-        while (!list.isEmpty()&&day.get()>0) {
-            temp = list.stream().filter(assignment -> assignment.d >= day.get()).sorted().collect(Collectors.toList());
 
-            if(!temp.isEmpty()&&temp.get(0).d>=day.get()) {
-                Assignment assignment = temp.remove(0);
-                list.remove(assignment);
-                sum+=assignment.w;
+        Collections.sort(assignmentList); // 과제를 점수가 높은 순으로 정렬
+
+
+        for(Assignment assignment : assignmentList) {
+            for(int day = assignment.due;day>0;day--) {
+                if(!occupied[day]) {
+                    occupied[day]= true;
+                    ans += assignment.weight;
+                    break;
+                }
             }
-            day.decrementAndGet();
         }
-        System.out.println(sum);
-
+        System.out.println(ans);
     }
 }
-
-
-//6 6 5 day --
-//5 day --;
-//4 4 60 day --
-//3 4 40 day --;
-//2 2 50 day--;
-
