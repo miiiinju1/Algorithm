8a5009dbd7cb5a7572d58b1c46954e95009f9591
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-01 16:39:07 +0900

    [Gold V] Title: 풍선 맞추기, Time: 920 ms, Memory: 175512 KB -BaekjoonHub

---
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            int now =Integer.parseInt(st.nextToken());
+            int next = now -1;
+            if(map.containsKey(now)) {
+                if (map.get(now) == 1) {
+                    map.remove(now);
+                } else {
+                    map.replace(now, map.get(now) - 1);
+                }
+            }
+            if(map.containsKey(next)) {
+                map.replace(next, map.get(next) + 1);
+            }
+            else {
+                map.put(next, 1);
+            }
+        }
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            sum += entry.getValue();
+        }
+        System.out.println(sum);
+    }
+}
