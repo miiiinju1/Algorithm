7661ee0e813c30b8408dec832774dd279f81142c
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-06 15:03:37 +0900

    [Gold V] Title: 전깃줄, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,33 +9,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] A = new int[501];
-        int[] B = new int[501];
         for(int i =0;i<N;i++) {
             String[] input = br.readLine().split(" ");
             int a = Integer.parseInt(input[0]);
             int b = Integer.parseInt(input[1]);
             A[a] = b;
-            B[b] = a;
         }
-        int max = 0;
         int[] dp = new int[501];
-        int[] dp2 = new int[501];
         for (int i = 0; i < 501; i++) {
             for (int j = i; j >= 0; j--) {
                 if (A[i] > A[j] && dp[i] < dp[j] + 1) {
                     dp[i] = dp[j] + 1;
                 }
-                if (B[i] > B[j] && dp2[i] < dp2[j] + 1) {
-                    dp2[i] = dp2[j] + 1;
-                }
+               
             }
         }
-        int temp = Arrays.stream(dp).max().getAsInt();
-        int temp2 = Arrays.stream(dp2).max().getAsInt();
-        max = Math.max(max, Math.max(temp, temp2));
+        int max = Arrays.stream(dp).max().getAsInt();
 
         System.out.println(N-max);
     }
 }
-
-//
