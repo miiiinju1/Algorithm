4a8664ef3a5b09cce94935e26ca623ce0b6d6ca5
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-07 19:29:19 +0900

    [Gold IV] Title: 치즈, Time: 160 ms, Memory: 15348 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,140 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Point {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Point point = (Point) o;
+
+            if (y != point.y) return false;
+            return x == point.x;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = y;
+            result = 31 * result + x;
+            return result;
+        }
+
+        int y, x;
+
+        public Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuffer sb = new StringBuffer("Point{");
+            sb.append("y=").append(y);
+            sb.append(", x=").append(x);
+            sb.append('}');
+            return sb.toString();
+        }
+    }
+
+    static int[][] map;
+
+
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, -1, 0, 1};
+    static int N, M;
+
+    static void erase(int val) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == -100) {
+                    map[i][j] = val;
+                }
+            }
+        }
+    }
+
+    static void bfsAir(int startY, int startX,int val) {
+        ArrayDeque<Point> q = new ArrayDeque<>();
+        q.add(new Point(startY, startX));
+        while (!q.isEmpty()) {
+            Point now = q.poll();
+
+
+            for (int i = 0; i < 4; i++) {
+                int y = dy[i] + now.y;
+                int x = dx[i] + now.x;
+                if (y >= 0 && x >= 0 && y < N && x < M&&map[y][x]!=-100 &&map[y][x] !=val) {
+                    if (map[y][x] == -1) {
+                        map[y][x] = -100;
+                        count++;
+                    } else {
+                        map[y][x]=val;
+                        q.add(new Point(y, x));
+                    }
+
+                }
+
+            }
+        }
+//        for(int i= 0;i<N;i++) {
+//            System.out.println();
+//            for (int j = 0; j < M; j++) {
+//                System.out.printf("%5d ",map[i][j]);
+//            }
+//        }
+
+    }
+
+
+
+    static int count = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                int temp = Integer.parseInt(st.nextToken());
+                if(temp==1) {
+                    temp = -1;
+                }
+                map[i][j] = temp;
+            }
+        }
+//        for(int i= 0;i<N;i++) {
+//            System.out.println();
+//            for (int j = 0; j < M; j++) {
+//                System.out.printf("%5d ",map[i][j]);
+//            }
+//        }
+        int sum = 1;
+        while (count > 0) {
+            int temp = count;
+            count = 0;
+//
+//            System.out.println();
+            bfsAir(0,0,sum);
+
+            if (count == 0) {
+
+                System.out.println(sum-1);
+                System.out.println(temp);
+                return;
+            }
+            erase(sum);
+            sum++;
+
+        }
+
+    }
+}
