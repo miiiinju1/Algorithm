28a6a1944d31e57ba998ce3a0b4e7ab916b47862
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-09 16:44:43 +0900

    [Gold II] Title: 로고, Time: 256 ms, Memory: 23928 KB -BaekjoonHub

--- 백준/Gold/3108. 로고/로고.java
+++ 백준/Gold/3108. 로고/로고.java
@@ -14,7 +14,7 @@ public class Main {
     }
     static int[] dy = {-1,0,1,0};
     static int[] dx = {0, 1, 0, -1};
-    static int[][] map = new int[2001][2001];
+    static boolean[][] map = new boolean[2001][2001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,20 +30,20 @@ public class Main {
             for (int i = y1; i <= y2; i++) {
                 if (i == y1 || i == y2) {
                     for (int j = x1; j <= x2; j++) {
-                        if(map[i][j]==0) {
-                            map[i][j] = 1;
+                        if(!map[i][j]) {
+                            map[i][j] = true;
                             count++;
                         }
                     }
                 }
                 else {
-                    if(map[i][x1]==0) {
-                        map[i][x1] = 1;
+                    if(!map[i][x1]) {
+                        map[i][x1] = true;
                         count++;
                     }
 
-                    if(map[i][x2]==0) {
-                        map[i][x2] = 1;
+                    if(!map[i][x2]) {
+                        map[i][x2] = true;
                         count++;
                     }
 
@@ -53,21 +53,21 @@ public class Main {
         ArrayDeque<Point> q = new ArrayDeque<>();
         q.add(new Point(1000,1000));
         long tempCount=0;
-        if(map[1000][1000]==1) {
+        if(map[1000][1000]) {
             while (!q.isEmpty()) {
                 Point now = q.poll();
 
-                if(map[now.y][now.x]!=1) {
+                if(!map[now.y][now.x]) {
                     continue;
                 }
-                map[now.y][now.x] = 0;
+                map[now.y][now.x] = false;
                 tempCount++;
 
                 for(int i= 0;i<4;i++) {
                     int y = now.y + dy[i];
                     int x = now.x + dx[i];
 
-                    if (y >= 0 && x >= 0 && y < 2001 && x < 2001 && map[y][x] == 1) {
+                    if (y >= 0 && x >= 0 && y < 2001 && x < 2001 && map[y][x]) {
                         q.add(new Point(y, x));
 
                     }
@@ -81,23 +81,24 @@ public class Main {
          long sum = 0;
         for(int i=0;i<2001;i++) {
             for(int j= 0;j<2001;j++) {
-                if(map[i][j]==1) {
+
+                if(map[i][j]) {
                     sum++;
                     q.add(new Point(i,j));
 
                     while(!q.isEmpty()) {
                         Point now = q.poll();
 
-                        if(map[now.y][now.x]!=1) {
+                        if(!map[now.y][now.x]) {
                             continue;
                         }
-                        map[now.y][now.x] = 0;
+                        map[now.y][now.x] = false;
                         tempCount++;
                         for(int d= 0;d<4;d++) {
                             int y = now.y+dy[d];
                             int x = now.x+dx[d];
 
-                            if (y >= 0 && x >= 0 && y < 2001 && x < 2001 && map[y][x]==1) {
+                            if (y >= 0 && x >= 0 && y < 2001 && x < 2001 && map[y][x]) {
                                 q.add(new Point(y, x));
 
                             }
