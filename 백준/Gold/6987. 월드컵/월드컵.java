02bae2284bef4c1cbea7ea769037ec5e6673e13d
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-16 23:52:27 +0900

    [Gold IV] Title: 월드컵, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -4,27 +4,30 @@ import java.util.StringTokenizer;
 public class Main {
     static int[][] game;
     static int[][] map;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
- static boolean flag;
-    static void dfs(int phase)  {
+    static boolean dfs(int phase) {
         if(phase==15) {
-            flag = true;
-            return ;
+            return true;
         }
+
         int a = game[phase][0];
         int b = game[phase][1];
+
         if(map[a][0]>0 && map[b][2]>0) {
             map[a][0]--;
             map[b][2]--;
-            dfs(phase + 1);
+            if(dfs(phase + 1)) {
+                return true;
+            }
             map[a][0]++;
             map[b][2]++;
         }
         if(map[a][1]>0 && map[b][1]>0) {
             map[a][1]--;
             map[b][1]--;
-            dfs(phase + 1);
+            if(dfs(phase + 1)) {
+                return true;
+            }
             map[a][1]++;
             map[b][1]++;
         }
@@ -32,15 +35,21 @@ public class Main {
         if(map[a][2]>0 && map[b][0]>0) {
             map[a][2]--;
             map[b][0]--;
-            dfs(phase + 1);
+            if(dfs(phase + 1)) {
+                return true;
+            }
             map[a][2]++;
             map[b][0]++;
         }
+        
+        return false;
     }
 
         public static void main (String[]args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            a:for (int tc = 0; tc < 4; tc++) {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            a:
+            for (int tc = 0; tc < 4; tc++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 map = new int[6][3];
                 game = new int[15][2];
@@ -56,26 +65,59 @@ public class Main {
                     for (int j = 0; j < 3; j++) {
                         map[i][j] = Integer.parseInt(st.nextToken());
                         temp += map[i][j];
+
                     }
                     if (temp != 5) {
                         bw.write("0 ");
                         continue a;
                     }
+
                 }
-                dfs(0);
-                if (flag) {
+                if (dfs(0)) {
                     bw.write("1 ");
+                    
                 }
                 else {
                     bw.write("0 ");
-
+                    
                 }
-                flag = false;
 
 
             }
-
+        
         bw.flush();
         bw.close();
     }
 }
+
+//5 4 3 2 1
+// 15
+// 15
+
+//또는
+//총 합은 일단 30이여야함
+
+
+// 0 0 2 4 3
+// 1 3 2
+// 0 2 2
+//0 1 1
+//
+
+
+//0 1 2 4 0 3
+
+//0 1 1 3 0 2
+
+// 0 0 1 2 0 2
+//5 0 0 3 0 2 2 0 3 0 0 5 4 0 1 1 0 4
+//4 1 0 3 0 2 4 1 0 1 1 3 0 0 5 1 1 3
+//5 0 0 4 0 1 2 2 1 2 0 3 1 0 4 0 0 5
+//5 0 0 3 1 1 2 1 2 2 1 3 0 0 5 1 0 4
+
+//0 1 2 3 3 4
+//0 0 0 1 2 1
+//5 4 3 1 0 0
+
+
+//5 0 0 4 0 3 3 0 2 1 1 1 0 2 3 0 1 4
