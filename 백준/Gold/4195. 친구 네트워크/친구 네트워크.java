360e9ebb20c3645eda108e6fd7bebffb8e72fdcc
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-20 16:58:31 +0900

    [Gold II] Title: 친구 네트워크, Time: 880 ms, Memory: 81044 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -3,60 +3,64 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    static HashMap<String, String> map;
+
+    static HashMap<String, String> parent;
+    static HashMap<String, Integer> size;
 
     static String find(String v) {
-        if(map.get(v).equals(v)) {
+        if (v.equals(parent.get(v))) {
             return v;
         }
         else {
-            map.replace(v,find(map.get(v)));
-            return map.get(v);
+            parent.replace(v, find(parent.get(v)));
+            return parent.get(v);
         }
     }
+
     static void union(String a, String b) throws IOException {
         String findA = find(a);
         String findB = find(b);
+
         if (!findA.equals(findB)) {
             if(size.get(findA)>size.get(findB)) {
                 String temp = findA;
                 findA = findB;
                 findB = temp;
             }
-            map.replace(findA, findB);
-            size.replace(findB, (size.get(findB)+size.get(findA)));
+            parent.replace(findA,findB);//A의 부모를 B로
+            size.replace(findB, size.get(findB)+size.get(findA));
+
+
         }
         bw.write(size.get(findB)+"\n");
-
     }
 
-    static HashMap<String, Integer> size = new HashMap<>();
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
 
         for(int tc = 0;tc<T;tc++) {
             int F = Integer.parseInt(br.readLine());
-
-            map = new HashMap<>();
-            for(int f = 0;f<F;f++) {
+            parent = new HashMap<>();
+            size = new HashMap<>();
+            for (int f = 0; f < F; f++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String a = st.nextToken();
                 String b = st.nextToken();
-                if(!map.containsKey(a)) {
-                    map.put(a, a);
-                    size.put(a, 1);}
-                if(!map.containsKey(b)) {
-                    map.put(b, b);
+                if (!parent.containsKey(a)) {
+                    parent.put(a, a);
+                    size.put(a, 1);
+                }
+                if (!size.containsKey(b)) {
+                    parent.put(b, b);
                     size.put(b, 1);
                 }
-                union(a,b);
+                union(a, b);
             }
+
         }
         bw.flush();bw.close();
     }
 }
-
