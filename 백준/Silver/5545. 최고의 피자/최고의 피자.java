de57270cae7c4af83f505a9eae640f0e357d77fe
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-23 14:42:50 +0900

    [Silver III] Title: 최고의 피자, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/5545. 최고의 피자/최고의 피자.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int dough = Integer.parseInt(br.readLine())*1000;
+
+        int max = dough/A;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int n = 0;n<N;n++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        int before = dough;
+
+        while(!pq.isEmpty()) {
+            Integer topping = pq.poll();
+            before+=(topping*1000);
+            A+=B;
+
+            if(before/A<=max) {
+                System.out.println(max/1000);
+
+                return ;
+            }
+            else {
+                max = before/A;
+            }
+            
+        }
+
+        System.out.println(max/1000);
+
+    }
+}
