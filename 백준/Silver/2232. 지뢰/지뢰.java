b2126e3228598909220a286b75010d57725fbd2b
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-28 18:51:37 +0900

    [Silver II] Title: 지뢰, Time: 280 ms, Memory: 21780 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.java
+++ 백준/Silver/2232. 지뢰/지뢰.java
@@ -5,23 +5,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
         int[] ary = new int[N+2];
-        ary[0] = 0;
-        ary[N+1]=0;
         for(int i = 1;i<=N;i++) {
             ary[i] = Integer.parseInt(br.readLine());
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i =1;i<=N;i++) {
             if(ary[i]>=ary[i-1]&&ary[i]>=ary[i+1]) {
-                pq.add(i);
+                bw.write(i+"\n");
             }
         }
-        while(!pq.isEmpty())  {
-            bw.write(pq.poll() + "\n");
-        }
+        
         bw.flush();bw.close();
     }
 
