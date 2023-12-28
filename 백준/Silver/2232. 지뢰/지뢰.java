b7d17083d9a6d07a16be4bd746373f89f4e8b6f0
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-12-28 18:50:26 +0900

    [Silver II] Title: 지뢰, Time: 348 ms, Memory: 25436 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.java
+++ 백준/Silver/2232. 지뢰/지뢰.java
@@ -1,74 +1,28 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class Main {
-    static class Mine {
-        int num, p;
-        public Mine(int num, int p) {
-            this.num = num;
-            this.p = p;
-        }
-    }
-   static int[] ary;
-    static void findLeft (int index) {
-        if(index>0 && ary[index]>ary[index-1]) {
-            findLeft(index-1);
-
-        }
-        ary[index] = Integer.MIN_VALUE;
-
-    }
-    static void findRight (int index) {
-
-        if(index<N && ary[index]>ary[index+1]) {
-            findRight(index+1);
-        }
-        ary[index] = Integer.MIN_VALUE;
-    }
-    static boolean find(int now, int index) {
-        
-        if(ary[index]==Integer.MIN_VALUE) {
-            return false;
-        }
-        findLeft(index);
-        ary[index] = now;
-        findRight(index);
-
-        return true;
-    }
-    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> result = new ArrayList<>();
-        ary = new int[N+1];
-
-        PriorityQueue<Mine> pq = new PriorityQueue<>((o1, o2) -> o2.p - o1.p);
-        for(int i= 1;i<=N;i++) {
+        int N = Integer.parseInt(br.readLine());
+        int[] ary = new int[N+2];
+        ary[0] = 0;
+        ary[N+1]=0;
+        for(int i = 1;i<=N;i++) {
             ary[i] = Integer.parseInt(br.readLine());
-            pq.add(new Mine(i, ary[i]));
         }
-        while (!pq.isEmpty()) {
-            final Mine mine = pq.poll();
-            if(find(mine.p, mine.num)) {
-                result.add(mine.num);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i =1;i<=N;i++) {
+            if(ary[i]>=ary[i-1]&&ary[i]>=ary[i+1]) {
+                pq.add(i);
             }
-//
         }
-
-
-        result.stream().sorted().forEach(c -> {
-            try {
-                bw.write(c+"\n");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        bw.flush();
-bw.close();
-
+        while(!pq.isEmpty())  {
+            bw.write(pq.poll() + "\n");
+        }
+        bw.flush();bw.close();
     }
+
 }
