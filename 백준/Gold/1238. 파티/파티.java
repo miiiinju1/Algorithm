30a48488d18ac6c0bd7c917777f1d37baf875b60
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-01-05 21:30:37 +0900

    [Gold III] Title: 파티, Time: 256 ms, Memory: 23824 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,9 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static final int MAX = Integer.MAX_VALUE/2;
+
+    static class Point implements Comparable<Point>{
+        int next,weight;
+
+        public Point(int next, int weight) {
+            this.next = next;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,8 +30,12 @@ public class Main {
         int X = Integer.parseInt(st.nextToken());
 
         int[][] map = new int[N + 1][N + 1];
+        int[][] reverseMap = new int[N + 1][N + 1];
         for(int i= 1;i<=N;i++) {
-            Arrays.fill(map[i],Integer.MAX_VALUE/2);
+            Arrays.fill(map[i],MAX);
+            Arrays.fill(reverseMap[i],MAX);
+            map[i][i] = 0;
+            reverseMap[i][i] = 0;
         }
 
         for(int i = 0;i<M;i++) {
@@ -25,38 +45,51 @@ public class Main {
             int weight = Integer.parseInt(st.nextToken());
 
             map[start][end] = weight;
+            reverseMap[end][start] = weight;
         }
-        for(int i = 1;i<=N;i++) {
-            map[i][i] = 0;
-        }
-        int max = 0;
-        for(int k = 1;k<=N;k++) {
-            
 
+        int[] value = new int[N + 1];
 
-                for (int i = 1; i <= N; i++) {
-                    if (map[i][k] != Integer.MAX_VALUE/2) {
-                    for (int j = 1; j <= N; j++) {
-                        map[i][j] = Math.min(map[i][k] + map[k][j], map[i][j]);
+        Arrays.fill(value, MAX);
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        pq.add(new Point(X, 0));
 
-                    }
+        while(!pq.isEmpty()) {
+            final Point now = pq.poll();
+
+            if(value[now.next]<now.weight) {
+                continue;
+            }
+            value[now.next] = now.weight;
+            for(int i =1 ;i<=N;i++) {
+                if(map[now.next][i]!=MAX&&value[i]>now.weight+map[now.next][i]) {
+                    pq.add(new Point(i, now.weight+map[now.next][i]));
                 }
             }
         }
 
-        for(int i= 1;i<=N;i++) {
-            if (map[i][X] != Integer.MAX_VALUE / 2) {
-                max = Math.max(max, map[i][X]+map[X][i]);
+        int[] value2 = new int[N + 1];
+        Arrays.fill(value2, MAX);
+        pq = new PriorityQueue<>();
+        pq.add(new Point(X, 0));
+
+        while(!pq.isEmpty()) {
+            final Point now = pq.poll();
+
+            if(value2[now.next]<now.weight) {
+                continue;
+            }
+            value2[now.next] = now.weight;
+            for(int i =1 ;i<=N;i++) {
+                if(reverseMap[now.next][i]!=MAX&&value2[i]>now.weight+reverseMap[now.next][i]) {
+                    pq.add(new Point(i, now.weight+reverseMap[now.next][i]));
+                }
             }
         }
+        int max = 0;
+        for(int i =1 ;i<=N;i++) {
+            max = Math.max(value[i] + value2[i], max);
+        }
         System.out.println(max);
-
-
-
-
-
-
     }
-
 }
-
