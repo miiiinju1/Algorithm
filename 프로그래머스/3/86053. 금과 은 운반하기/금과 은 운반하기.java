a4e9941549feca2017b607cef2c669634502bc8b
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-01-31 18:51:26 +0900

    [level 3] Title: 금과 은 운반하기, Time: 0.16 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -1,51 +1,32 @@
-
-
 class Solution {
-
-    static int[] silver;
-    static int[] gold;
-    static int[] weight;
-    static int[] time;
-    
-    //ㅅㅂ 이걸 어케푸노
-    static boolean Check(long now,int a, int b) {
-        long totalGold=0;
-        long totalSilver=0;
-        long total = 0;
-        for(int i= 0;i<weight.length;i++) {
-            long count = now/(time[i]*2);
-            
-            if(now%(time[i]*2)>=time[i]) {
-                count++;
-            }
-            
-            totalGold+= Math.min(gold[i],weight[i]*count);
-            totalSilver+= Math.min(silver[i],weight[i]*count);
-            total+= Math.min(gold[i]+silver[i], weight[i]*count);
-        }
-        if(a<=totalGold && b <= totalSilver && a+b <= total) {
-            return true;
-        }
-        return false;
-        
-    }
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        silver = s;
-        gold = g;
-        weight = w;
-        time = t;
-        long lo = 0,hi = 1000000000000001L;
-        while(lo+1<hi) {
-            long mid = (hi-lo)/2 + lo;
-            
-            if(Check(mid,a,b)) {
-                hi = mid;
-            }
-            else {
-                lo = mid;
-            }
-        }
+        //10 ^ 9 * 10^5 * 4
+        long lo = 0, hi = 400000000000000L;
         
+        while(lo+1<hi) {
+            long now = (hi-lo)/2+lo;
+            
+            long totalGold = 0;
+            long totalSilver = 0;
+            long total = 0;
+            
+            for(int i = 0;i<g.length;i++) {
+                long count = now / (t[i]*2);
+                
+                if(now%(t[i]*2)>=t[i]) {
+                    count++;
+                }
+                totalGold += Math.min(g[i], w[i]*count);
+                totalSilver += Math.min(s[i], w[i]*count);
+                total+= Math.min(w[i]*count, g[i]+s[i]);
+            }
+            if(totalGold>=a && totalSilver>=b&&total>=a+b) {
+                hi = now;
+            }else {
+                lo = now;
+            }
+            
+        }
         return hi;
     }
 }
