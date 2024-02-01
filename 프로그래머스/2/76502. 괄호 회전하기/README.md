3ee96945bf40071a0e84968c95e1b3c8934d5df6
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-02-01 20:30:55 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.20 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#qna) 
 
 ### 성능 요약
 
-메모리: 76.5 MB, 시간: 0.27 ms
+메모리: 72.1 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 20:27:37
+2024년 2월 4일 20:30:51
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,16 +2,12 @@ import java.util.*;
 class Solution {
     static boolean Check(char[] chars) {
         Stack<Character> stack = new Stack();
-        int c = 0;
-        // System.out.println("start");
         for(int i= 0;i<chars.length;i++) {
             if(!stack.isEmpty()&&stack.peek()==chars[i])  {
                 stack.pop();
-                c++;
                 continue;
             }
             else if(stack.isEmpty()&&(chars[i] == ')'||chars[i]==']'||chars[i]=='}')) {
-                    // System.out.println("Cant");
                     return false;
             }
             else if(chars[i]=='(') {
@@ -24,11 +20,7 @@ class Solution {
             else if (chars[i] == '{') {
                 stack.add('}');
             }
-            // System.out.print(i+" "+chars[i]+" : ");
-            // for(char a : stack) {
-            //     System.out.print(a+" ");
-            // }
-            // System.out.println();
+            
             
         }
         if(stack.size()==0) {
