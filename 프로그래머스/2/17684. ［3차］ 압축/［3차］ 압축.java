117e02e34c7edd2e750d2cc08b2fafbc3e517be2
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-02-08 16:42:48 +0900

    [level 2] Title: [3차] 압축, Time: 2.50 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,51 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String msg) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i= 0;i<26;i++) {
+            // System.out.println((char)(65+i));
+            map.put(String.valueOf((char)(65+i)), i+1);
+        }
+        
+       
+        int index = 27;
+        
+         StringBuilder sb = new StringBuilder() ;
+        ArrayList<Integer> sum = new ArrayList<>();
+        for(int i= 0 ;i<msg.length();i++) {
+            
+            sb = new StringBuilder();
+            int j= i;
+            for(;j<msg.length();j++) {
+                sb.append(msg.charAt(j));
+                if(map.containsKey(sb.toString())) {
+           
+                    if(msg.equals("ABABABABABABABAB"))
+                    System.out.println(sb);
+                }
+                else {
+                    map.put(sb.toString(),index++);
+                    sb.deleteCharAt(sb.length()-1);
+                    break;
+                }
+                
+            }
+                    i = j-1;
+           
+            
+                sum.add(map.get(sb.toString()));
+            
+        }
+        
+        
+        
+        
+        
+        
+       
+        
+        return sum.stream().mapToInt(i-> i).toArray();
+    }
+}
