425bacc392159270c9734067f83b8c0d4919b9ab
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-02-17 16:39:38 +0900

    [Gold II] Title: 물대기, Time: 328 ms, Memory: 24360 KB -BaekjoonHub

--- 백준/Gold/1368. 물대기/물대기.java
+++ 백준/Gold/1368. 물대기/물대기.java
@@ -5,16 +5,13 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int find(int v) {
         if (parent[v] == v) {
             return v;
         }
-
         parent[v] = find(parent[v]);
         return parent[v];
     }
-
     static boolean union(int a, int b) {
         int fa = find(a);
         int fb = find(b);
@@ -32,23 +29,11 @@ public class Main {
     static class Node implements Comparable<Node> {
         int a, b,weight;
 
-        @Override
-        public String toString() {
-            final StringBuffer sb = new StringBuffer("Node{");
-            sb.append("a=").append(a);
-            sb.append(", b=").append(b);
-            sb.append(", weight=").append(weight);
-            sb.append('}');
-            return sb.toString();
-        }
-
         public Node (int a, int b, int w) {
             this.a = a;
             this.b = b;
             this.weight =w ;
-
         }
-
         @Override
         public int compareTo(Node o) {
             return Integer.compare(this.weight, o.weight);
@@ -60,36 +45,30 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         PriorityQueue<Node> pq = new PriorityQueue<>();
         parent = new int[N + 1];
-        int[] weight = new int[N];
+
         for(int i= 1;i<=N;i++) {
             parent[i] = i;
-        }
-        for(int i= 1;i<=N;i++) {
-//            weight[i] = Integer.parseInt(br.readLine());
             pq.add(new Node(0, i, Integer.parseInt(br.readLine())));
         }
-        int[][] map = new int[N][N];
 
-        for(int i= 0;i<N;i++) {
+        for(int i= 1;i<=N;i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j= 0;j<N;j++) {
-
-//                map[i][j] = Integer.parseInt(st.nextToken());
-                int temp = Integer.parseInt(st.nextToken());
+            for(int j= 1;j<=N;j++) {
                 if(i<j) {
-                    pq.add(new Node(i+1, j+1, temp));
+                    pq.add(new Node(i, j, Integer.parseInt(st.nextToken())));
+                }
+                else {
+                    st.nextToken();
                 }
             }
         }
         long sum = 0;
-
         while(!pq.isEmpty()) {
             Node node = pq.poll();
 
             if(union(node.a,node.b)) {
                 sum += node.weight;
             }
-
         }
         System.out.println(sum);
 
