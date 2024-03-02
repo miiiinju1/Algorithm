d111c62a69346c25a29f02e7dd137967f2011e1d
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-03-02 12:31:46 +0900

    [level 3] Title: 여행경로, Time: 4.06 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Solution {
+    static HashMap<String,Integer> visited = new HashMap<>();
+    static ArrayList<String> result = new ArrayList<>();
+    static int size;
+    static boolean search(String now) {
+        if(0==visited.size()) {
+            return true;
+        }
+        for(String next : map.get(now)) {
+            if(visited.containsKey(now+next)) {
+                
+                visited.replace(now+next,visited.get(now+next)-1);
+                if(visited.get(now+next)==0) {
+                    visited.remove(now+next);
+                }
+                
+                if(search(next)) {
+                    result.add(next);
+                    
+                    return true;
+                }
+                visited.put(now+next, visited.getOrDefault(now+next, 0)+1);
+
+                
+            }
+        }
+        return false;
+    }
+    static HashMap<String, ArrayList<String>> map = new HashMap<>(); 
+    public String[] solution(String[][] tickets) {
+        //그냥 앞에서부터 계속 접근하면 되는거아닌가
+        size = tickets.length;
+        for(String[] ticket : tickets) {
+            String from = ticket[0];
+            String to = ticket[1];
+            
+            if(!map.containsKey(from)) {
+                map.put(from, new ArrayList<>());
+            }
+            if(!map.containsKey(to)) {
+                map.put(to, new ArrayList<>());
+            }
+            visited.put(from+to, visited.getOrDefault(from+to, 0)+1);
+            map.get(from).add(to);
+        }
+        
+        for(Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+            
+            Collections.sort(entry.getValue());
+        
+        }
+        search("ICN");
+        result.add("ICN");
+        Collections.reverse(result);
+        
+        String[] answer = {};
+        return result.stream().toArray(String[]::new);
+    }
+}
