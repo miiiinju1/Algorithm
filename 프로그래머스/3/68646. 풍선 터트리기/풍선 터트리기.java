cc959a50d309954bb9615b93c9c26541723d2724
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-03-04 18:14:24 +0900

    [level 3] Title: 풍선 터트리기, Time: 257.50 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,36 +1,22 @@
+import java.util.*;
 class Solution {
     public int solution(int[] a) {
-        int count = 0;
         if(a.length == 1) {
             return 1;
         }
-        boolean[] visited = new boolean[a.length];
+        HashSet<Integer> count = new HashSet<>();
         int now = Integer.MAX_VALUE;
 
         for(int i = 0;i<a.length;i++) {
-            
-            if(a[i]<now) {
-                if(!visited[i]) {
-                    count++;
-                    visited[i] = true;
-                }
-               now = a[i];
-            }
-            
+            now = Math.min(now, a[i]);
+            count.add(now);
         }
         now = Integer.MAX_VALUE;
         for(int i = a.length-1;i>=0;i--) {
-            
-            if(a[i]<now) {
-               if(!visited[i]) {
-                    count++;
-                    visited[i] = true;
-                }
-               now = a[i];
-            }
-            
+            now = Math.min(now, a[i]);
+            count.add(now);
         }
     
-        return count;
+        return count.size();
     }
 }
