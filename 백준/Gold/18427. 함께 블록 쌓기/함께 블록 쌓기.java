a4e5d1a373f755b70ccd620d77a25e2300dc5bcf
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-04-26 14:34:00 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 152 ms, Memory: 14648 KB -BaekjoonHub

--- 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static List<Integer>[] ary;
-    
+
     static int N, H;
 
     public static void main(String[] args) throws IOException {
@@ -19,26 +18,33 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
          H = Integer.parseInt(st.nextToken());
 
-        ary = new List[N];
+         int[][] ary = new int[N][M+1];
         for(int i = 0;i<N;i++) {
-            ary[i] = new ArrayList<>();
             st = new StringTokenizer(br.readLine());
-
-            ary[i].add(0);
+            int j = 0;
+            ary[i][j++]=0;
             while(st.hasMoreTokens()) {
-                ary[i].add(Integer.parseInt(st.nextToken()));
+                ary[i][j++]=Integer.parseInt(st.nextToken());
+            }
+            for(;j<=M;j++) {
+                ary[i][j] = -1;
             }
         }
 
         int[][] dp = new int[N][H+1];
-        for (Integer i : ary[0]) {
-            dp[0][i] += 1;
+        for (int i = 0;i<=M;i++) {
+            if(ary[0][i]==-1)
+                break;
+            dp[0][ary[0][i]] += 1;
         }
 
         for(int i = 1;i<N;i++) {
             for(int j = 0;j<=H;j++) {
                 if(dp[i-1][j]!=0) {
-                    for (Integer num : ary[i]) {
+                    for(int t = 0;t<=M;t++) {
+                        int num = ary[i][t];
+                        if(num==-1)
+                            break;
                         if(j+num<=H) {
                             dp[i][j+num] = (dp[i][j+num]+dp[i-1][j])%10007;
                         }
