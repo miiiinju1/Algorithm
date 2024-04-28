62bea0888b1252caf3274ee53b732aa857eb941c
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-04-28 13:53:58 +0900

    [Gold III] Title: 양팔저울, Time: 140 ms, Memory: 18248 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -1,40 +1,48 @@
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Reader rd = new Reader();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = rd.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int[] weights = new int[N];
-        int[][] dp = new int[80001][N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = 0;
         for(int i= 0;i<N;i++) {
-            weights[i] = rd.nextInt();
+            weights[i] = Integer.parseInt(st.nextToken());
+            max+=weights[i];
         }
 
-        dp[40000][N-1] = 1;
-        dp[weights[N-1]+40000][N-1] = 1;
-        dp[40000-weights[N-1]][N-1] = 1;
-        for(int i = N-2;i>=0;i--) {
-            for(int j= 0;j<=80000;j++) {
-                if(dp[j][i+1]==0) continue;
+        int dpMax = max*2;
+        int[] dp = new int[dpMax+1];
+        dp[max] = 1;
+        dp[weights[0] + max] = 1;
+        dp[max - weights[0]]= 1;
+        for(int i = 1;i<N;i++) {
+            int[] newDp = new int[dpMax + 1];
+            for(int j= 0;j<=dpMax;j++) {
+                if(dp[j]==0) continue;
 
-                dp[j][i] = 1;
+                newDp[j] = 1;
                 if(j-weights[i]>=0) {
-                    dp[j-weights[i]][i] = 1;
+                    newDp[j-weights[i]] = 1;
                 }
-                if(j+weights[i]<=80000) {
-                    dp[j+weights[i]][i] = 1;
+                if(j+weights[i]<=dpMax) {
+                    newDp[j+weights[i]] = 1;
                 }
             }
+            dp = newDp;
         }
 
-        int M = rd.nextInt();
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for(int i = 0;i<M;i++) {
-            if(dp[rd.nextInt()+40000][0]==1) {
+            int input = Integer.parseInt(st.nextToken())+max;
+
+
+            if(input<=dpMax &&dp[input]==1) {
                 bw.write("Y ");
             }
             else {
@@ -44,72 +52,5 @@ public class Main {
         bw.flush();bw.close();
     }
 
-    static class Reader {
-        final private int BUFFER_SIZE = 1 << 16;
-        private DataInputStream din;
-        private byte[] buffer;
-        private int bufferPointer, bytesRead;
 
-        public Reader() {
-            din = new DataInputStream(System.in);
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public String readLine() throws IOException {
-            byte[] buf = new byte[64]; // line length
-            int cnt = 0, c;
-            while ((c = read()) != -1) {
-                if (c == '\n') {
-                    if (cnt != 0) {
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-                buf[cnt++] = (byte) c;
-            }
-            return new String(buf, 0, cnt);
-        }
-
-        public int nextInt() throws IOException {
-            int ret = 0;
-            byte c = read();
-            while (c <= ' ') {
-                c = read();
-            }
-            boolean neg = (c == '-');
-            if (neg) {
-                c = read();
-            }
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            if (neg) {
-                return -ret;
-            }
-            return ret;
-        }
-
-        private void fillBuffer() throws IOException {
-            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
-            if (bytesRead == -1) {
-                buffer[0] = -1;
-            }
-        }
-
-        private byte read() throws IOException {
-            if (bufferPointer == bytesRead) {
-                fillBuffer();
-            }
-            return buffer[bufferPointer++];
-        }
-
-        public void close() throws IOException {
-            if (din != null) {
-                din.close();
-            }
-        }
-    }
 }
