6baefad8b1a254e0f68eba6880a31e7f0848696c
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-04-28 13:37:26 +0900

    [Gold III] Title: 양팔저울, Time: 220 ms, Memory: 27816 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -1,21 +1,18 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    static int[] weights;
-
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Reader rd = new Reader();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-
-        weights = new int[N];
-
+        int N = rd.nextInt();
+        int[] weights = new int[N];
         int[][] dp = new int[80001][N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i= 0;i<N;i++) {
-            weights[i] = Integer.parseInt(st.nextToken());
+            weights[i] = rd.nextInt();
         }
 
         dp[40000][N-1] = 1;
@@ -35,10 +32,9 @@ public class Main {
             }
         }
 
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        int M = rd.nextInt();
         for(int i = 0;i<M;i++) {
-            if(dp[Integer.parseInt(st.nextToken())+40000][0]==1) {
+            if(dp[rd.nextInt()+40000][0]==1) {
                 bw.write("Y ");
             }
             else {
@@ -46,5 +42,74 @@ public class Main {
             }
         }
         bw.flush();bw.close();
+    }
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din != null) {
+                din.close();
+            }
+        }
     }
 }
