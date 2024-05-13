8cf762a94e99e2a63fe3b64811ddc8b3370cea93
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-05-13 18:39:15 +0900

    [Gold I] Title: 같은 수로 만들기 2, Time: 188 ms, Memory: 18312 KB -BaekjoonHub

--- 백준/Gold/13146. 같은 수로 만들기 2/같은 수로 만들기 2.java
+++ 백준/Gold/13146. 같은 수로 만들기 2/같은 수로 만들기 2.java
@@ -1,17 +1,16 @@
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        Reader reader = new Reader();
+        int N = reader.nextInt();
 
         int max = 0;
         int[] ary = new int[N];
 
         for(int i= 0;i<N;i++) {
-            int temp = Integer.parseInt(br.readLine());
+            int temp = reader.nextInt();
             max = Math.max(max, temp);
             ary[i] = temp;
         }
@@ -19,18 +18,79 @@ public class Main {
         long sum = max - ary[0];
 
         for (int i = 0; i < N - 1; i++) {
-            if (ary[i] < ary[i + 1]) {
-
-            }
             if (ary[i] > ary[i + 1]) {
-//                sum += max - ary[i];
-//                sum+=1;
                 sum += ary[i] - ary[i + 1];// + (max - ary[i]);
             }
-//            else {
-//
-//            }
         }
         System.out.println(sum);
+    }
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din != null) {
+                din.close();
+            }
+        }
     }
 }
