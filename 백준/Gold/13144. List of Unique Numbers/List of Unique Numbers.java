dbe932ff87becb1f004b02d56399794396fb1d76
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-05-14 15:17:44 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 156 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -1,43 +1,97 @@
-
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-//        Map<Integer, Integer> window = new HashMap<>();
+        Reader reader = new Reader();
+        int N = reader.nextInt();
 
         long before = N;
         int[] beforeAry = new int[100001];
         Arrays.fill(beforeAry, -1);
-        beforeAry[Integer.parseInt(st.nextToken())] = 0;
+        beforeAry[reader.nextInt()] = 0;
 
-//        window.put(Integer.parseInt(st.nextToken()), 0);
         int lastIndex = -1;
         for (int i = 1; i < N; i++) {
-            int now = Integer.parseInt(st.nextToken());
-
-
+            int now = reader.nextInt();
+            
             if(beforeAry[now]!=-1) {
                 lastIndex = Math.max(lastIndex, beforeAry[now]);
             }
             beforeAry[now] = i;
-
-//            window.put(now, i);
-
             before = before + (i - lastIndex - 1);
 
         }
-
         System.out.println(before);
+    }
 
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
 
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din != null) {
+                din.close();
+            }
+        }
     }
 }
