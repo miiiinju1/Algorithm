04af532c0c99b3d78ef55dcb3a6bb6e09c0aa164
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-05-14 15:16:21 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 308 ms, Memory: 24988 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -1,8 +1,8 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,32 +12,30 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Map<Integer, Integer> window = new HashMap<>();
+//        Map<Integer, Integer> window = new HashMap<>();
 
         long before = N;
-        window.put(Integer.parseInt(st.nextToken()), 0);
+        int[] beforeAry = new int[100001];
+        Arrays.fill(beforeAry, -1);
+        beforeAry[Integer.parseInt(st.nextToken())] = 0;
+
+//        window.put(Integer.parseInt(st.nextToken()), 0);
         int lastIndex = -1;
         for (int i = 1; i < N; i++) {
             int now = Integer.parseInt(st.nextToken());
 
-            if(window.containsKey(now)) {
-                lastIndex = Math.max(lastIndex, window.get(now));
-                window.replace(now, i);
-            }
-            else {
-                window.put(now, i);
-            }
 
-//            System.out.println("lastIndex = " + lastIndex);
-//            System.out.println(before);
-//            if(i-lastIndex!=1)
+            if(beforeAry[now]!=-1) {
+                lastIndex = Math.max(lastIndex, beforeAry[now]);
+            }
+            beforeAry[now] = i;
+
+//            window.put(now, i);
+
             before = before + (i - lastIndex - 1);
 
         }
-//        for(int i= 0;i<N;i++) {
-//            System.out.print(dp[i]+" ");
-//        }
-//        System.out.println();
+
         System.out.println(before);
 
 
