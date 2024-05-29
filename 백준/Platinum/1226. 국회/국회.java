d918fe9b688a638fe21af52d425034b242f0b37a
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-05-29 13:25:52 +0900

    [Platinum IV] Title: 국회, Time: 316 ms, Memory: 54852 KB -BaekjoonHub

--- 백준/Platinum/1226. 국회/국회.java
+++ 백준/Platinum/1226. 국회/국회.java
@@ -45,45 +45,61 @@ public class Main {
         boolean[][] dp = new boolean[100001][N+1];
 //        dp[0] = true;
 
+        int[][] prev = new int[100001][2];
+
+        for(int i= 0;i<100001;i++) {
+            prev[i][0] = Integer.MAX_VALUE;
+            prev[i][1] = Integer.MAX_VALUE;
+
+        }
+
+
         value[0]= true;
 //        dp[0][0] = true;
         for (int i = 0; i < collect.size(); i++) {
 //            System.out.println(collect.get(i).num);
             for (int j = half; j >= 0; j--) {
                 if (value[j]) {
-//                    System.out.println(j+" "+collect.get(i).count);
-                    value[j + collect.get(i).count] = true;
-
-                    for (int z = 0; z <= N; z++) {
-                        dp[j + collect.get(i).count][z] = dp[j][z];
+                    int next = j+collect.get(i).count;
+                    value[next] = true;
+                    if (prev[next][0] == Integer.MAX_VALUE) {
+                        prev[next][0] = j;
                     }
-                    dp[j + collect.get(i).count][collect.get(i).num] = true;
+                    if(prev[next][1]==Integer.MAX_VALUE) {
+                        prev[next][1] = collect.get(i).num;
+                    }
+//                    dp[j + collect.get(i).count][collect.get(i).num] = true;
                 }
             }
         }
-//
-//        for(int i = 0;i<20;i++) {
-//            System.out.print(dp[i]+" ");
-//        }
         int max = 0;
         for(int i= 100000;i>=0;i--) {
-
             if(value[i]) {
                 max=i;
                 break;
             }
         }
-
 //        System.out.println("max = " + max);
         StringBuilder sb = new StringBuilder();
         int count = 0;
-        for(int i = 0;i<=N;i++) {
-//            System.out.println(dp[max][i]+" ");
-            if(dp[max][i]) {
-                ++count;
-                sb.append(i).append(" ");
-            }
+        int track = max;
+
+
+        while(true) {
+            sb.append(prev[track][1]).append(" ");
+            ++count;
+            track = prev[track][0];
+//            System.out.println(prev[track][0] + " " + prev[track][1]);
+            if(track==0)
+                break;
         }
+//        for(int i = 0;i<=N;i++) {
+////            System.out.println(dp[max][i]+" ");
+//            if(dp[max][i]) {
+//                ++count;
+//                sb.append(i).append(" ");
+//            }
+//        }
 
         System.out.println(count);
         System.out.println(sb);
