8a67f4f46bdbb106855732a2248692398f45d811
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-06-06 13:45:31 +0900

    [Platinum III] Title: 격자판 채우기, Time: 160 ms, Memory: 27096 KB -BaekjoonHub

--- 백준/Platinum/1648. 격자판 채우기/격자판 채우기.java
+++ 백준/Platinum/1648. 격자판 채우기/격자판 채우기.java
@@ -7,48 +7,48 @@ import java.util.StringTokenizer;
 public class Main {
     static int N, M;
     static int[][] dp;
-    static int MOD = 9901;
+    static final int MOD = 9901;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        dp = new int[N*M+1][1<<M];
+        dp = new int[N * M + 1][1 << M];
 
         dp[0][0] = 1;
 
         for (int i = 0; i < N * M; i++) {
 
-            for (int bit = 0; bit < 1<< M; bit++) {
-                if(dp[i][bit]==0) continue;
+            for (int mask = 0; mask < (1 << M); mask++) {
 
-                // 현재 칸이 채워져있는 게 있으면
-                if((bit & 1)==1) {
+                //0이 현재 칸,
+                //그러면 M-1번째 칸이 마지막칸임
+                if(dp[i][mask]==0) continue;
 
-                    // 그대로 옮기기
-                    dp[i + 1][bit >> 1] = (dp[i + 1][bit >> 1] + dp[i][bit]) % MOD;
+                if((mask & 1) > 0) {
+                    dp[i + 1][mask >> 1] = (dp[i + 1][mask >> 1] + dp[i][mask]) % MOD;
                 }
                 else {
-                    // 세로로 타일을 놓는 경우
-                    if (i / M != N - 1) {
-                        dp[i + 1][(bit >> 1) | (1 << (M - 1))] =
-                                (dp[i + 1][(bit >> 1) | (1 << (M - 1))] + dp[i][bit]) % MOD;
+                    //가로로 채워보기
+                    // 오른쪽 끝 줄 아니면
+                    if (i % M != M - 1) {
+                        // 현재칸은 비어있고,
+                        // 오른쪽 칸 비어있으면
+                        if((mask & 2) == 0) {
+                            dp[i + 2][mask >> 2] = (dp[i + 1][mask >> 2] + dp[i][mask]) % MOD;
+                        }
                     }
-                    // 가로로 타일을 놓는 경우
-                    if (i % M != M - 1 && (bit & 2) == 0) {
-                        dp[i + 2][bit >> 2] =
-                                (dp[i + 2][bit >> 2] + dp[i][bit]) % MOD;
+                    //세로로 채워보기
+                    //맨 아랫줄 아니면
+                    if (i / M != N - 1) {
+                        dp[i + 1][(mask >> 1) + (1 << (M - 1))] = (dp[i + 1][(mask >> 1) + (1 << (M - 1))] + dp[i][mask]) % MOD;
                     }
                 }
-
-
-
             }
 
         }
 
         System.out.println(dp[N*M][0]);
-
 
 
     }
