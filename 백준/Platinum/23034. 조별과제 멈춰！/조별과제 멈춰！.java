022fd8640d721f88b649cd7227e7c0e549c229e8
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-06-30 14:50:39 +0900

    [Platinum V] Title: 조별과제 멈춰!, Time: 756 ms, Memory: 97540 KB -BaekjoonHub

--- 백준/Platinum/23034. 조별과제 멈춰！/조별과제 멈춰！.java
+++ 백준/Platinum/23034. 조별과제 멈춰！/조별과제 멈춰！.java
@@ -1,5 +1,8 @@
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
@@ -17,12 +20,12 @@ public class Main {
     static int[][] cost;
     static int N,M;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Reader reader = new Reader();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+//        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = reader.nextInt();
+        M = reader.nextInt();
 
         parent = new int[N + 1];
         cost = new int[N + 1][N + 1];
@@ -35,11 +38,11 @@ public class Main {
         }
 
         for(int i = 0;i<M;i++) {
-            st = new StringTokenizer(br.readLine());
+//            st = new StringTokenizer(br.readLine());
 
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
+            int A = reader.nextInt();
+            int B = reader.nextInt();
+            int C = reader.nextInt();
 
             pq.add(new Edge(A, B, C));
         }
@@ -58,18 +61,17 @@ public class Main {
         }
 
         // 여기에서 모든 쌍에 대해 뺼 거 적어두기
-        // 반까지만 하고 나중에 찾을 때 순서 바꿔서 검색하면 줄일 수 있음
         for (int i = 1; i <= N; i++) {
             Arrays.fill(cost[i], Integer.MAX_VALUE);
             bfs(i, cost[i]);
         }
 
-        int Q = Integer.parseInt(br.readLine());
+        int Q = reader.nextInt();
         for (int i = 0; i < Q; i++) {
-            st = new StringTokenizer(br.readLine());
-            int X = Integer.parseInt(st.nextToken());
-            int Y = Integer.parseInt(st.nextToken());
-
+//            st = new StringTokenizer(br.readLine());
+            int X = reader.nextInt();
+            int Y = reader.nextInt();
+            
             bw.write(sum - cost[X][Y] + "\n");
 
         }
@@ -137,6 +139,50 @@ public class Main {
             return true;
         }
         return false;
+    }
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din != null) {
+                din.close();
+            }
+        }
     }
 
 
