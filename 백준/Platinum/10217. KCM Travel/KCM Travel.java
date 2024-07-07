c0aaca8cfa5475247c3e8f20317837f48c5fac55
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-07-07 14:46:37 +0900

    [Platinum IV] Title: KCM Travel, Time: 1864 ms, Memory: 44272 KB -BaekjoonHub

--- 백준/Platinum/10217. KCM Travel/KCM Travel.java
+++ 백준/Platinum/10217. KCM Travel/KCM Travel.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+//dp 풀이
 
+public class Main {
     static class Edge {
         int to, cost, time;
 
@@ -18,15 +19,48 @@ public class Main {
 
     static Map<Integer, ArrayList<Edge>> map = new HashMap<>();
 
+    static int[][] dp;
+
+    static int dfs(int now, int cost) {
+        if(cost > M) {
+            return Integer.MAX_VALUE;
+        }
+        if (now == N) {
+            dp[now][cost] = 0;
+            return 0;
+        }
+        if (dp[now][cost] != -1) {
+            return dp[now][cost];
+        }
+        int minTime = Integer.MAX_VALUE;
+        for (Edge edge : map.get(now)) {
+            int nextCost = edge.cost + cost;
+            if(nextCost <= M) {
+                int nextTime = dfs(edge.to, nextCost);
+                if (nextTime != Integer.MAX_VALUE) {
+                    minTime = Math.min(minTime, nextTime + edge.time);
+                }
+            }
+        }
+        dp[now][cost] = minTime;
+        return minTime;
+    }
+
+
+    static int N,M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+         N = Integer.parseInt(st.nextToken());
+         M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        dp = new int[N + 1][M + 1];
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
 
         for (int i = 1; i <= N; i++) {
             map.put(i, new ArrayList<>());
@@ -43,73 +77,16 @@ public class Main {
                 map.get(u).add(new Edge(v, c, d));
         }
 
-        for(int i= 1;i<=N;i++) {
-            map.get(i).sort((o1, o2) -> {
-                if(o1.time==o2.time) {
-                    return Integer.compare(o1.cost, o2.cost);
-                }
-                return Integer.compare(o1.time, o2.time);
-            });
-        }
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(1, 0, 0));
-
-        int[][] time = new int[N + 1][M + 1];
-        for(int i= 0;i<=N;i++)
-        Arrays.fill(time[i], Integer.MAX_VALUE);
-        time[1][0] = 0;
-
-        while(!pq.isEmpty()) {
-            final Node now = pq.poll();
-
-            for (Edge edge : map.get(now.to)) {
-                int nextCost = now.cost + edge.cost;
-                int nextTime = now.time + edge.time;
-                if (nextCost <= M&&time[edge.to][nextCost] > nextTime) {
-                    for (int i = nextCost; i <= M; i++) {
-                        if (nextTime < time[edge.to][i]) {
-                            time[edge.to][i] = nextTime;
-                        } else {
-                            break;
-                        }
-                    }
-                    pq.add(new Node(edge.to, now.cost + edge.cost, now.time + edge.time));
 
 
-
-                }
-
-            }
-        }
-
-        int min = Integer.MAX_VALUE;
-        for(int i= 0;i<=M;i++) {
-            min = Math.min(min, time[N][i]);
-        }
-
-        if (min == Integer.MAX_VALUE) {
+        int ans = dfs(1, 0);
+        if(ans==Integer.MAX_VALUE) {
             System.out.println("Poor KCM");
             return;
         }
-        System.out.println(min);
+        System.out.println(ans);
 
 
 
-
-    }
-    static class Node implements Comparable<Node> {
-        int to, cost, time;
-
-        public Node(int to, int cost, int time) {
-            this.to = to;
-            this.cost = cost;
-            this.time = time;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.time, o.time);
-        }
     }
 }
