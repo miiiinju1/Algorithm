ec8cacfd75e65c0656057e743c4da6e27ceaa43f
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-13 23:56:37 +0900

    [Gold IV] Title: 일루미네이션, Time: 244 ms, Memory: 22828 KB -BaekjoonHub

--- 백준/Gold/5547. 일루미네이션/일루미네이션.java
+++ 백준/Gold/5547. 일루미네이션/일루미네이션.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-  static int[][] map, check;
+  static int[][] map;
   static int extendH, extendW;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +17,7 @@ public class Main {
     int H = Integer.parseInt(st.nextToken());
 
     extendW = 4 * W + 3;
-     extendH = 6 * H + 8;
+    extendH = 6 * H + 8;
     map = new int[extendH+2][extendW+2];
 
     for(int i = 0;i<H;i++) {
@@ -35,26 +34,18 @@ public class Main {
           for(int z = x-1;z<=x+1;z++) {
             map[y+1][z]=1;
           }
-//          map[y+1][x-1] = 1;
-//          map[y+1][x+1] =1;
           for(int z = x-2;z<=x+2;z++) {
             map[y+2][z]=1;
           }
-//          map[y + 2][x - 2] = 1;
-//          map[y + 2][x + 2] = 1;
           for(int z = x-2;z<=x+2;z++) {
             map[y+3][z]=1;
           }
-//          map[y + 3][x - 2] = 1;
-//          map[y + 3][x + 2] = 1;
           for(int z = x-2;z<=x+2;z++) {
             map[y+4][z]=1;
           }
           for(int z = x-1;z<=x+1;z++) {
             map[y+5][z]=1;
           }
-//          map[y + 4][x - 1] = 1;
-//          map[y + 4][x + 1] = 1;
           map[y + 6][x] = 1;
         }
       }
@@ -68,26 +59,18 @@ public class Main {
           for(int z = x-1;z<=x+1;z++) {
             map[y+1][z]=1;
           }
-//          map[y+1][x-1] = 1;
-//          map[y+1][x+1] =1;
           for(int z = x-2;z<=x+2;z++) {
             map[y+2][z]=1;
           }
-//          map[y + 2][x - 2] = 1;
-//          map[y + 2][x + 2] = 1;
           for(int z = x-2;z<=x+2;z++) {
             map[y+3][z]=1;
           }
-//          map[y + 3][x - 2] = 1;
-//          map[y + 3][x + 2] = 1;
           for(int z = x-2;z<=x+2;z++) {
             map[y+4][z]=1;
           }
           for(int z = x-1;z<=x+1;z++) {
             map[y+5][z]=1;
           }
-//          map[y + 4][x - 1] = 1;
-//          map[y + 4][x + 1] = 1;
           map[y + 6][x] = 1;
         }
 
@@ -95,9 +78,6 @@ public class Main {
 
 
     }
-
-    check = new int[extendH+2][extendW+2];
-
     visited = new boolean[extendH+2][extendW+2];
     Deque<Point> q = new ArrayDeque<>();
 
@@ -109,7 +89,6 @@ public class Main {
       final Point now = q.poll();
 
       if (map[now.y][now.x] == 1) {
-        check[now.y][now.x] = 1;
         m++;
         continue;
       }
@@ -127,43 +106,10 @@ public class Main {
       }
     }
     System.out.println(m/2);
-//    visited = new boolean[extendH+2][extendW+2];
-//
-//    int result = 0;
-//    for(int i =0;i<extendH+2;i++) {
-//      for(int j= 0;j<extendW+2;j++) {
-//        if(check[i][j]==1 && !visited[i][j]) {
-////          visited[i][j] = true;
-//          count = -1;
-//          dfs(i, j, 0, -1, -1);
-//          result += count/2;
-//        }
-//      }
-//    }
-//
-//    System.out.println(result);
+
 
   }
   static boolean[][] visited;
-  static int count = 0;
-
-  static void dfs(int y, int x, int c, int befY, int befX) {
-//    System.out.println("y + \" \" + x = " + y + " " + x);
-    count++;
-    
-    for(int d = 0;d<8;d++) {
-      int Y = dy[d] + y;
-      int X = dx[d] + x;
-
-      if (Y >= 0 && X >= 0 && Y <= extendH + 1 && X <= extendW + 1
-          && check[Y][X]==1&& (befY!=Y || befX!=X) && !visited[Y][X]) {
-        
-        visited[Y][X] = true;
-        dfs(Y, X, c + 1, y, x);
-
-      }
-    }
-  }
 
   static int[] dy = {-1, 0, 1, 0, -1, -1, 1, 1};
   static int[] dx = {0, -1, 0, 1, 1, -1, 1, -1};
