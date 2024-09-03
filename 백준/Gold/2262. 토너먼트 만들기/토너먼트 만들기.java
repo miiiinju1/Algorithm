c035d0fe2e262578c9dd073157e11455ad3ecada
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-09-03 17:35:54 +0900

    [Gold IV] Title: 토너먼트 만들기, Time: 148 ms, Memory: 16664 KB -BaekjoonHub

---
+++ 백준/Gold/2262. 토너먼트 만들기/토너먼트 만들기.java
@@ -0,0 +1,74 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Candidate implements Comparable<Candidate> {
+        int point;
+        int winner;
+        int loser;
+        int cost;
+
+        public Candidate(int point, int a, int b, int cost) {
+            this.point = point;
+            this.winner = Math.min(a, b);
+            this.loser = Math.max(a, b);
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Candidate o) {
+            if (this.winner == o.winner) {
+                return Integer.compare(this.cost, o.cost);
+            }
+            return Integer.compare(o.winner, this.winner);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        var st = new StringTokenizer(br.readLine());
+
+        int[] map = new int[n];
+        for(int i= 0;i<n;i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+
+        PriorityQueue<Candidate> pq;
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0;i<n;i++) {
+            list.add(map[i]);
+        }
+
+        long sum = 0L;
+        while (list.size() > 1) {
+            pq = new PriorityQueue<>();
+            for (int j = 1; j < list.size(); j++) {
+                pq.add(new Candidate(j - 1, list.get(j - 1), list.get(j),
+                    Math.abs(list.get(j - 1) - list.get(j))));
+            }
+            Candidate candidate = pq.poll();
+            sum += candidate.cost;
+            ArrayList<Integer> next = new ArrayList<>();
+            for (int i = 0; i < list.size(); i++) {
+                if (i == candidate.point) {
+                    next.add(candidate.winner);
+                } else if (i != (candidate.point + 1)) {
+                    next.add(list.get(i));
+                }
+            }
+            list = next;
+        }
+        System.out.println(sum);
+
+    }
+
+}
