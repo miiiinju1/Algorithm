b5503a5b06cd7679e49d8b773b8b4f02834a5bcf
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-09-13 20:23:06 +0900

    Time: 4 ms (29.03%), Space: 42.1 MB (19.65%) - LeetHub

---
+++ 1449-print-words-vertically/1449-print-words-vertically.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public List<String> printVertically(String s) {
+        String[] words = s.split(" ");
+
+        int maxLength = 0;
+        for (String word : words) {
+            maxLength = Math.max(word.length(), maxLength);
+        }
+
+        char[][] result = new char[maxLength][words.length];
+        for (char[] chars : result) {
+            Arrays.fill(chars, ' ');
+        }
+
+        for(int i =0;i<words.length;i++) {
+            String word = words[i];
+            for(int j= 0;j<word.length();j++) {
+                result[j][i] = word.charAt(j);
+            }
+        }
+        for(int i = 0;i<result.length;i++) 
+        System.out.println(Arrays.toString(result[i]));
+
+        return Arrays.stream(result)
+            .map(ary -> new String(ary).stripTrailing())
+            .toList();
+
+        
+
+
+
+        // HOWWW
+        // ARE
+        // YOU
+
+        // HAY
+        // ..
+        // W
+        // W
+        // return null;
+    }
+}
