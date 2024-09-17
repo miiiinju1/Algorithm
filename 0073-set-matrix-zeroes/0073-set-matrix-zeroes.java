5ba9eacd8f22ba3d17888f9fe05f767b7b360834
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-09-17 18:53:26 +0900

    Time: 2 ms (18.93%), Space: 45.4 MB (70.56%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,31 @@
+import java.util.*;
 class Solution {
     public void setZeroes(int[][] matrix) {
 
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean[] markCol = new boolean[m];
-        boolean[] markRow = new boolean[n];
+        
+        // boolean[] markCol = new boolean[m];
+        // boolean[] markRow = new boolean[n];
+
+        Set<Integer> markCol = new HashSet<>();
+        Set<Integer> markRow = new HashSet<>();
 
         for(int i = 0;i<m;++i) {
 
             for(int j= 0;j<n;++j) {
                 if(matrix[i][j]==0) {
-                    markRow[j] = true;
-                    markCol[i] = true;
+                    markRow.add(j);
+                    markCol.add(i);
+                    // markRow[j] = true;
+                    // markCol[i] = true;
                 }    
             }
         }
 
         for(int i = 0;i<m;++i) {
             for(int j = 0;j<n;++j) {
-                if(markCol[i] || markRow[j]) {
+                if(markCol.contains(i) || markRow.contains(j)) {
                     matrix[i][j] = 0;
                 }
             }
