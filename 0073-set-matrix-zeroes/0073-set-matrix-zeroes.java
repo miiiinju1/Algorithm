a121999e0b43459de92bc96c5d4994803055d929
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-09-17 18:54:22 +0900

    Time: 1 ms (78.44%), Space: 45.9 MB (9.51%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,27 +5,28 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         
-        // boolean[] markCol = new boolean[m];
-        // boolean[] markRow = new boolean[n];
+        boolean[] markCol = new boolean[m];
+        boolean[] markRow = new boolean[n];
 
-        Set<Integer> markCol = new HashSet<>();
-        Set<Integer> markRow = new HashSet<>();
+        // Set<Integer> markCol = new HashSet<>();
+        // Set<Integer> markRow = new HashSet<>();
 
         for(int i = 0;i<m;++i) {
 
             for(int j= 0;j<n;++j) {
                 if(matrix[i][j]==0) {
-                    markRow.add(j);
-                    markCol.add(i);
-                    // markRow[j] = true;
-                    // markCol[i] = true;
+                    // markRow.add(j);
+                    // markCol.add(i);
+                    markRow[j] = true;
+                    markCol[i] = true;
                 }    
             }
         }
 
         for(int i = 0;i<m;++i) {
             for(int j = 0;j<n;++j) {
-                if(markCol.contains(i) || markRow.contains(j)) {
+                // if(markCol.contains(i) || markRow.contains(j) {
+                if(markCol[i] || markRow[j]) {
                     matrix[i][j] = 0;
                 }
             }
