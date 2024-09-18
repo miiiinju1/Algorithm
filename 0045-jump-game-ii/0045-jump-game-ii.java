4691d44434ec6eb40ebe104be62b5ad0d58e63a4
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-09-18 11:56:44 +0900

    Time: 3 ms (26.55%), Space: 46.3 MB (6.11%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,22 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
         if(nums.length==1) {
-
             return 0;
-
-
         }
-         int[] ary = new int[nums.length];
+        int[] ary = new int[nums.length];
         for(int i= 0;i<nums.length;++i) {
             int max = Math.min(nums[i]+i, nums.length);
             ary[i] = max;
         }
         //        farest? = [2,4,3,4,4];
-
         // 첫 지점부터 가면서 maxIndex를 갱신시켜가면서 canGoFar이란 값이 nums.length-1이 될 때까지 반복한다?
-
-
-        
         int count = 1;
         int canGoFar = Math.min(nums.length-1, ary[0]);
         if(canGoFar == nums.length-1) {
@@ -30,13 +23,10 @@ class Solution {
                 canGoFar = Math.max(canGoFar, Math.min(nums.length-1,nums[i] + i));
             }
             ++count;
-
             if(canGoFar == nums.length-1) {
                 break;
             }
         }
-
         return count;
-
     }
 }
